28a691cff646dc5017176a7adc765aeeaa8bdc5d
Chen Yu Pao <windperson@gmail.com>
2017-11-03 18:41:34 +0800

    Demo how to call Async Task in Command

--- XamarinFormCommand/XamarinFormCommand/ViewModel/MyViewModel.cs
+++ XamarinFormCommand/XamarinFormCommand/ViewModel/MyViewModel.cs
@@ -30,7 +30,8 @@ namespace XamarinFormCommand.ViewModel
 
         public ICommand SquareRootCommand { get; private set; }
         public ICommand SquareRootWithParameterCommand { get; private set; }
-        
+        public ICommand SimulateDownloadCommand { get; private set; }
+
         #endregion
 
 
@@ -48,6 +49,7 @@ namespace XamarinFormCommand.ViewModel
             Number = 25;
             SquareRootCommand = new Command(CalculateSquerRoot);
             SquareRootWithParameterCommand = new Command<string>(CalculateSquerRoot);
+            SimulateDownloadCommand = new Command(async () => await SimulateDownloadAsync(), () => canDownload);
         }
 
 
@@ -64,6 +66,53 @@ namespace XamarinFormCommand.ViewModel
             double num = Convert.ToDouble(value);
             SquareRootWithParameterResult = Math.Sqrt(num);
             OnPropertyChanged("SquareRootWithParameterResult");
+        }
+
+        #endregion
+
+        #region Async Command Demo
+        bool canDownload = true;
+        string simulatedDownloadResult;
+
+        public string SimulatedDownloadResult
+        {
+            get { return simulatedDownloadResult; }
+            private set
+            {
+                if (simulatedDownloadResult != value)
+                {
+                    simulatedDownloadResult = value;
+                    OnPropertyChanged("SimulatedDownloadResult");
+                }
+            }
+        }
+
+        async Task SimulateDownloadAsync()
+        {
+            CanInitiateNewDownload(false);
+            SimulatedDownloadResult = string.Empty;
+            await Task.Run(() => SimulateDownload());
+            SimulatedDownloadResult = "Simulated download complete";
+            CanInitiateNewDownload(true);
+        }
+
+        void CanInitiateNewDownload(bool value)
+        {
+            canDownload = value;
+            ((Command)SimulateDownloadCommand).ChangeCanExecute();
+        }
+
+        void SimulateDownload()
+        {
+            // Simulate a 5 second pause
+            var endTime = DateTime.Now.AddSeconds(5);
+            while (true)
+            {
+                if (DateTime.Now >= endTime)
+                {
+                    break;
+                }
+            }
         }
 
         #endregion
